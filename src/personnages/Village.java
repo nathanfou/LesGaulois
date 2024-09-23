9962eec358a50419e4d29e0874de8cf489aba4b0
Nathan Bayle <bln4454a@ensfsi.onmicrosoft.com>
2024-09-23 19:54:21 +0200

    moitié tp2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,10 +2,41 @@ package personnages;
 
 public class Village {
 	private String nom;
-	public Village(String nom) {
+	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+	private int nbVillageoisMaximum;
+	
+	public Village(String nom, int nbVillageoisMaximum ) {
+		this.nbVillageoisMaximum = nbVillageoisMaximum;
 		this.nom = nom;
+		villageois = new Gaulois[nbVillageoisMaximum];
 	}
+	
+
 	public String getNom() {
 		return nom;
+		
 	}
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		if (nbVillageois < nbVillageoisMaximum) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois++;
+		}
+	}
+
+	public Gaulois trouverHabitant(int nbVillageois) {
+		return villageois[nbVillageois];
+		
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des irréductibles",30);
+		
+	}
+	
 }
